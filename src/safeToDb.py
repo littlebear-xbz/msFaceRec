79f8713d0c58a27ff591ce64341ff88e80d7b8eb
熊北 <littlebear.xbz@qq.com>
2017-10-20 16:52:58 +0800

    新增多线程，处理phoenixDb连接超时问题

--- src/safeToDb.py
+++ src/safeToDb.py
@@ -10,9 +10,10 @@ from kafka import KafkaConsumer
 import phoenixdb
 import logging
 import hashlib
-import datetime
+import time
 import ConfigParser
 import pymysql
+import threading
 CF = ConfigParser.ConfigParser()
 CF.read('../conf/conf.conf')
 
@@ -89,8 +90,6 @@ def safeToPhoenix(data,cursor,conn):
         logging.info("safe To Phoenix:" + sql_phoenix)
         cursor.execute(sql_phoenix)
 
-
-
 def safeToMysql(data,cursor,conn):
     recived_message = data
     messagelist = recived_message.split(",")
@@ -155,39 +154,55 @@ def safeToMysql(data,cursor,conn):
         cursor.execute(sql_mysql)
         conn.commit()
 
-
-
-if __name__ == "__main__":
-    print "process start"
-    bootstrap_servers = CF.get("kafka","bootstrap").split(",")
-    kafka_topic = CF.get("kafka","topic_reply")
-    kafka_group_id = CF.get("kafka","group_id")
-    kafka_consumer = KafkaConsumer(kafka_topic,group_id=kafka_group_id,bootstrap_servers=bootstrap_servers)
-    phoenix_url = CF.get("phoenix","url")
-    phoenix_conn = phoenixdb.connect(phoenix_url,max_retries=3,autocommit=True)
-    phoenix_cursor = phoenix_conn.cursor()
-
-    mysql_conn = pymysql.connect(host=CF.get("mysql","ip"),port= int(CF.get("mysql","port")),\
-                                 user = CF.get("mysql","username"),\
-                                 passwd=CF.get("mysql","password"),\
-                                 db=CF.get("mysql","database"),charset='utf8')
+def kafkaToDb(phoenix_cursor,phoenix_conn):
+    bootstrap_servers = CF.get("kafka", "bootstrap").split(",")
+    kafka_topic = CF.get("kafka", "topic_reply")
+    kafka_group_id = CF.get("kafka", "group_id")
+    kafka_consumer = KafkaConsumer(kafka_topic, group_id=kafka_group_id, bootstrap_servers=bootstrap_servers)
+    mysql_conn = pymysql.connect(host=CF.get("mysql", "ip"), port=int(CF.get("mysql", "port")),
+                                 user=CF.get("mysql", "username"),
+                                 passwd=CF.get("mysql", "password"),
+                                 db=CF.get("mysql", "database"), charset='utf8')
     mysql_cursor = mysql_conn.cursor()
 
     for message in kafka_consumer:
         print message.value
         logging.debug("Recived message value from kafka topic ::" + message.value)
-        #通过phoenix 插入数据到hbase
-        if CF.get("phoenix","enable") == "True":
-            safeToPhoenix(data=message.value,cursor=phoenix_cursor,conn=phoenix_conn)
+        # 通过phoenix 插入数据到hbase
+        if CF.get("phoenix", "enable") == "True":
+            safeToPhoenix(data=message.value, cursor=phoenix_cursor, conn=phoenix_conn)
             print "safe to phoenix done"
         else:
             logging.debug("safe to Phoenix is no enable")
-        #插入数据到mysql
-        if CF.get("mysql","enable") == "True":
-            safeToMysql(message.value,cursor=mysql_cursor,conn=mysql_conn)
+        # 插入数据到mysql
+        if CF.get("mysql", "enable") == "True":
+            safeToMysql(message.value, cursor=mysql_cursor, conn=mysql_conn)
             print  "safe to mysql done"
         else:
             logging.debug("safe to mysql is no enable")
+
+def keepConn(cur):
+    while True:
+        cur.execute("select 1")
+        logging.debug("select 1")
+        print "keep alive"
+        time.sleep(200)
+
+def main():
+    print "process start"
+    phoenix_url = CF.get("phoenix", "url")
+    phoenix_conn = phoenixdb.connect(phoenix_url, max_retries=3, autocommit=True)
+    phoenix_cursor = phoenix_conn.cursor()
+    kafkaToDb_thread = threading.Thread(target=kafkaToDb,args=(phoenix_cursor,phoenix_conn,))
+    keepConn_thread = threading.Thread(target=keepConn,args=(phoenix_cursor,))
+    kafkaToDb_thread.start()
+    keepConn_thread.start()
+    kafkaToDb_thread.join()
+    keepConn_thread.join()
+
+if __name__ == "__main__":
+    main()
+
 
 
 
