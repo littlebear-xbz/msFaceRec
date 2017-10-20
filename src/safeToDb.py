ef8349a88e314b0ddc4b8c607a69a52e64a97aa0
熊北 <littlebear.xbz@qq.com>
2017-10-20 16:58:36 +0800

    处理mysql 插入主键问题

--- src/safeToDb.py
+++ src/safeToDb.py
@@ -110,7 +110,7 @@ def safeToMysql(data,cursor,conn):
         recived_status = messagelist[1]
         # print str(messagelist)
         rowkey = hashlib.md5(recived_url_send + recived_time).hexdigest()
-        sql_mysql = """INSERT INTO ODS_MSFACEREC_RECIVED(RowSets,send_url,recived_time,status)
+        sql_mysql = """REPLACE INTO ODS_MSFACEREC_RECIVED(RowSets,send_url,recived_time,status)
                         VALUES('%(rowkey)s',
                         '%(recived_url_send)s',
                         '%(date)s',
@@ -135,7 +135,7 @@ def safeToMysql(data,cursor,conn):
         logging.debug("list lenth:::" + str(len(messagelist)))
         for i in range(3, len(messagelist)):
             recived_results[i - 3] = messagelist[i]
-        sql_mysql = """INSERT INTO ODS_MSFACEREC_RECIVED(RowSets,send_url,recived_time,status,result_1,result_2,result_3
+        sql_mysql = """REPLACE INTO ODS_MSFACEREC_RECIVED(RowSets,send_url,recived_time,status,result_1,result_2,result_3
                 ,result_4,result_5,result_6,result_7,result_8,result_9,result_10)
                 VALUES('%(rowkey)s',
                 '%(recived_url_send)s',
